bb10f05ab564e98e44806268169a389b18de853f
Zofia <zosiazmojdzin@gmail.com>
2014-06-14 10:29:56 +0200

    error and success messages

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,20 @@
 class TodosController < ApplicationController
   def index
   	 @todo_items = Todo.all
+  	 @new_todo = Todo.new
+  end
+
+  def add
+   todo = Todo.create(:todo_item => params[:todo][:todo_item])
+
+    unless todo.valid?
+     flash[:error] = todo.errors.full_messages.join("<br>").html_safe
+    else
+     flash[:success] = "Todo item added successfully"   
+    end 
+
+  redirect_to :action => 'index'
+
   end
 
   def delete
